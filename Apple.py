a53ecf7a1fffba9ce93b96073721aa9ad8fea7f9
Houssem Ben Mabrouk <cjhoussem@gmail.com>
2017-08-23 23:55:04 +0100

    Initial commit

---
+++ Apple.py
@@ -0,0 +1,19 @@
+from constraints import *
+from random import randrange
+
+class Apple:
+    x = 0
+    y = 0
+    radius = 20
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def pickLocation(self, snake):
+        self.x = randrange(self.radius*2, screenWidth-self.radius*2, self.radius)
+        self.y = randrange(self.radius*2, screenHeight-self.radius*2, self.radius)
+        for i in range(0, snake.total, 1):
+            if self.x == snake.x[i] and self.y == snake.y[i]:
+                print("Picking a new apple location..")
+                self.pickLocation(snake)
---
+++ setup.py
@@ -0,0 +1,16 @@
+import cx_Freeze
+
+cx_Freeze.setup(
+    name="Snake Game",
+    version="0.1",
+    options={"build_exe": {"include_files": ["Sprites/grass_tile.png",
+                                             "Sprites/wall_tile.png",
+                                             "Sprites/apple_sprite.png",
+                                             "Sprites/snake_head_sprite.png",
+                                             "Sprites/snake_body_tile.png",
+                                             "Sounds/apple_eaten.wav",
+                                             "Sounds/gameover.wav",
+                                             "Sounds/playing.wav",
+                                             "Sounds/menu.wav"]}},
+    executables=[cx_Freeze.Executable("snake_game.py")]
+)
